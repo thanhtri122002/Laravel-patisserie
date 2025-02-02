89d2539d48ed80d7ebed9923e6dbc0cf53626c52
thanhtri122002 <93241140+thanhtri122002@users.noreply.github.com>
2025-02-02 19:38:28 +0700

    finish landing page version 1

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -39,11 +39,16 @@
     </div>
     <div id="mobileMenu" class="mobile-menu">
         <div class="flex m-10 justify-between">
-            <img class="w-auto h-[2rem] object-cover" src="{{ asset('storage/images/icons/patisserie.svg') }}" alt="">
-            <button id="closeBtn">x</button>
+            <img class="w-auto h-[3rem] object-cover" src="{{ asset('storage/images/icons/patisserie.svg') }}" alt="">
+            <button id="closeBtn">
+                <svg width="28px" height="28px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M14.5 9.50002L9.5 14.5M9.49998 9.5L14.5 14.5" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+                    <path class="round" d="M7 3.33782C8.47087 2.48697 10.1786 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 10.1786 2.48697 8.47087 3.33782 7" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+                </svg>
+            </button>
         </div>
         
-        <ul>
+        <ul class="ml-[1rem] flex flex-col gap-3">
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#services">Services</a></li>
