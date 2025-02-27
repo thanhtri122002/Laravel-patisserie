cf988e328f76d61b5a77c6841539c1808bd2540c
thanhtri122002 <93241140+thanhtri122002@users.noreply.github.com>
2025-02-27 15:33:03 +0700

    add our teams page

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
-<div class="xs-container mx-auto rounded-[1.5rem] py-[5.5rem]">
+<div class="xs-container mx-auto py-[5.5rem] md:rounded-[1.5rem]">
     <div class="xxs-container mx-auto">
-        <div class="upper-footer flex flex-col items-start justify-between md:flex-row md:items-end">
+        <div class="upper-footer flex flex-col items-start justify-around md:flex-row">
             <div class="flex flex-col gap-3">
                 <p class="text-h3 font-mer">Get in Touch</p>
                 <p class="text-h3 font-mer">Stay Connect With Anywhere</p>
@@ -11,8 +11,24 @@
                     <p class="text-h3">Glamour</p>
                 </div>
             </div>
-            
+            <div class="grid grid-cols-2 gap-6">
+                <div class="flex flex-col gap-5">
+                    <p>LINKS</p>
+                    <div class="flex flex-col gap-3">
+                        <a href="/">Home</a>
+                        <a href="/teams">Team</a>
+                    </div>
+                    
+                </div>
+                <div class="flex flex-col gap-5">
+                    <p>CONTACTS</p>
+                    <div class="flex flex-col gap-3">
+                        <a href="/">Home</a>
+                        <a href="/teams">Team</a>
+                    </div>
+                </div>                
+            </div>
         </div>
-        <div class=""></div>
     </div>
+    
 </div>
