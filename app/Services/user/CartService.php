9392bf41a06b7080985a0b8378cbcd01d7a3ee2c
Nguyen Tri Thanh <tts-thanhnt@bekisoft.com>
2025-07-03 19:40:01 +0700

    changcartcost

--- app/Services/user/CartService.php
+++ app/Services/user/CartService.php
@@ -196,19 +196,6 @@ class CartService extends Service
         return true;
     }
 
-    public function getCartCost()
-    {
-        $cart = $this->getOrCreateCart();
-        
-        $productDetails = $cart->productDetails;
-        $cost = 0;
-        foreach($productDetails as $detail) {
-            $cost = $detail->calculateTotal();
-        }
-
-        return $cost;
-    }
-
     /**
      * A function to get the total price of the cart
      * 
