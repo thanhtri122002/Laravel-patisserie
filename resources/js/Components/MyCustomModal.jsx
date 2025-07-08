e516ed891b4a625113f96b332c6546c5a5e93bcb
Nguyen Tri Thanh <tts-thanhnt@bekisoft.com>
2025-07-08 10:08:56 +0700

    upgragde the sidebarnavigation

--- resources/js/Components/MyCustomModal.jsx
+++ resources/js/Components/MyCustomModal.jsx
@@ -2,11 +2,44 @@ import { Transition, Dialog, DialogPanel } from "@headlessui/react";
 import { createContext, useContext, useState } from "react";
 
 
-/**
- * 
- * 
- */
-function MyCusTomModal ({children, show=false, closable= true, onClose = () => {}}) {
+const ModalContext = createContext();
 
+const Modal = ({ children }) => {
+    const [ open, setIsOpen ] = useState(false);
+
+    const toggleModal = () => {
+        setIsOpen((prevState) => !prevState);
+    }
+    
+    return (
+        <ModalContext.Provider value={{open, setIsOpen, toggleModal }}>
+            {children}
+        </ModalContext.Provider>
+    )
 }
 
+const Trigger = ({ children , ...props }) => {
+    const { open , setIsOpen, toggleModal} = useContext(ModalContext);
+
+    return (
+        <div {...props} onClick={toggleModal}>{children}</div>
+    )
+}
+
+const Content = ({children, ...props}) => {
+    const { open, setIsOpen, toggleModal } = useContext(ModalContext);
+    
+    return (
+        <Transition
+            show={open}
+            enter="transition-opacity ease-out duration-200"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity ease-in duration-150"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+        >
+            <div className='fixed inset-0 bg-black opacity-35 z-100'></div>
+        </Transition>
+    )
+}
