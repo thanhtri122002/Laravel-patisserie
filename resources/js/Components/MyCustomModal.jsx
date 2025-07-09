9260eb56da7283dcf2b6f110cca77fd9547c2de9
thanhtri122002 <93241140+thanhtri122002@users.noreply.github.com>
2025-07-09 09:07:20 +0700

    cart content modal for the mobile

--- resources/js/Components/MyCustomModal.jsx
+++ resources/js/Components/MyCustomModal.jsx
@@ -1,45 +1,73 @@
-import { Transition, Dialog, DialogPanel } from "@headlessui/react";
-import { createContext, useContext, useState } from "react";
+import { Transition, TransitionChild } from "@headlessui/react";
+import { createContext, useContext } from "react";
 
 
 const ModalContext = createContext();
 
-const Modal = ({ children }) => {
-    const [ open, setIsOpen ] = useState(false);
-
-    const toggleModal = () => {
-        setIsOpen((prevState) => !prevState);
-    }
+const Modal = ({ children, open, setIsOpen, toggleOpen }) => {
     
     return (
-        <ModalContext.Provider value={{open, setIsOpen, toggleModal }}>
+        <ModalContext.Provider value={{open, setIsOpen, toggleOpen }}>
             {children}
         </ModalContext.Provider>
     )
 }
 
 const Trigger = ({ children , ...props }) => {
-    const { open , setIsOpen, toggleModal} = useContext(ModalContext);
+    const { toggleOpen } = useContext(ModalContext);
 
     return (
-        <div {...props} onClick={toggleModal}>{children}</div>
+        <>
+            <div {...props} onClick={toggleOpen}>{children}</div>
+            {open && (
+                <div
+                    className="fixed inset-0 z-40"
+                    onClick={() => setOpen(false)}
+                ></div>
+            )}
+        </>
     )
 }
 
-const Content = ({children, ...props}) => {
-    const { open, setIsOpen, toggleModal } = useContext(ModalContext);
-    
+const Content = ({ children }) => {
+    const { open, setIsOpen } = useContext(ModalContext);
+    console.log(open);
     return (
-        <Transition
-            show={open}
-            enter="transition-opacity ease-out duration-200"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity ease-in duration-150"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+      <Transition show={open}>
+        <TransitionChild
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-            <div className='fixed inset-0 bg-black opacity-35 z-100'></div>
-        </Transition>
-    )
-}
+          <div
+            onClick={() => setIsOpen(false)}
+            className="fixed inset-0 bg-black/50"
+          />
+        </TransitionChild>
+  
+        <TransitionChild
+          enter="ease-out duration-300"
+          enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          enterTo="opacity-100 translate-y-0 sm:scale-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+          leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        >
+          <div className="fixed inset-0 flex items-center justify-center p-4">
+            <div className="bg-white rounded-lg shadow-xl p-6 w-full max-w-md">
+              {children}
+            </div>
+          </div>
+        </TransitionChild>
+      </Transition>
+    );
+  };
+  
+
+Modal.Trigger = Trigger;
+Modal.Content = Content;
+
+export default Modal;
